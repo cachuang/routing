2310a162764fa858fc1ae30ab42a7fcb3ddd94bc
cachuang <cachuang@cs.nctu.edu.tw>
2018-06-09 22:36:10 +0800

    Support multiple number of leaf/spine for trellisp4.py

--- trellis/trellisp4.py
+++ trellis/trellisp4.py
@@ -22,37 +22,50 @@ from bmv2 import ONOSBmv2Switch
 
 PIPECONF_ID = 'org.onosproject.pipelines.fabric'
 
+LEAF_START_INDEX = 204
+SPINE_START_INDEX = 226
+LEAF_GRPC_PORT_START = 55204
+SPINE_GRPC_PORT_START = 55226
+
+nleaf = 2
+nspine = 2
+nhost = 2
+
 class Trellis( Topo ):
     "Trellis basic topology"
 
     def __init__( self, *args, **kwargs ):
         Topo.__init__( self, *args, **kwargs )
 
-        # Spines
-        s226 = self.addSwitch('s226', cls=ONOSBmv2Switch, deviceId='226', grpcport=55226, pipeconf=PIPECONF_ID, portcfg=True)
-        s227 = self.addSwitch('s227', cls=ONOSBmv2Switch, deviceId='227', grpcport=55227, pipeconf=PIPECONF_ID, portcfg=True)
+        leafSwitches = []
+        spineSwitches = []
 
-        # Leaves
-        s204 = self.addSwitch('s204', cls=ONOSBmv2Switch, deviceId='204', grpcport=55204, pipeconf=PIPECONF_ID, portcfg=True)
-        s205 = self.addSwitch('s205', cls=ONOSBmv2Switch, deviceId='205', grpcport=55205, pipeconf=PIPECONF_ID, portcfg=True)
+        for n in range(nleaf):
+            self.addSwitch("s" + str(LEAF_START_INDEX + n), 
+                cls=ONOSBmv2Switch, deviceId=str(LEAF_START_INDEX + n), grpcport=LEAF_GRPC_PORT_START + n, 
+                pipeconf=PIPECONF_ID, portcfg=True)
+            leafSwitches.append("s" + str(LEAF_START_INDEX + n))
 
-        # Switch Links
-        self.addLink(s226, s204)
-        self.addLink(s226, s205)
-        self.addLink(s227, s204)
-        self.addLink(s227, s205)
+        for n in range(nspine):
+            self.addSwitch("s" + str(SPINE_START_INDEX + n), 
+                cls=ONOSBmv2Switch, deviceId=str(SPINE_START_INDEX + n), grpcport=SPINE_GRPC_PORT_START + n, 
+                pipeconf=PIPECONF_ID, portcfg=True)
+            spineSwitches.append("s" + str(SPINE_START_INDEX + n))
 
-        # NOTE avoid using 10.0.1.0/24 which is the default subnet of quaggas
+        for leaf in leafSwitches:
+            for spine in spineSwitches:
+                self.addLink(leaf, spine)
+
+        # NOTE avoid using t=10.0.1.0/24 which is the default subnet of quaggas
         # NOTE avoid using 00:00:00:00:00:xx which is the default mac of host behind upstream router
         # IPv4 Hosts
-        h1 = self.addHost('h1', cls=RoutedHost, mac='00:aa:00:00:00:01', ips=['10.0.2.1/24'], gateway='10.0.2.254')
-        h2 = self.addHost('h2', cls=RoutedHost, mac='00:aa:00:00:00:02', ips=['10.0.2.2/24'], gateway='10.0.2.254')
-        h3 = self.addHost('h3', cls=RoutedHost, mac='00:aa:00:00:00:03', ips=['10.0.3.1/24'], gateway='10.0.3.254')
-        h4 = self.addHost('h4', cls=RoutedHost, mac='00:aa:00:00:00:04', ips=['10.0.3.2/24'], gateway='10.0.3.254')
-        self.addLink(h1, s204)
-        self.addLink(h2, s204)
-        self.addLink(h3, s205)
-        self.addLink(h4, s205)
+        for m in range(nleaf):
+            for n in range(nhost):
+                mac = "00:aa:00:00:00:" + str(m*nhost+n+1).zfill(2)
+                ip = "10.0." + str(m+2) + "." + str(n+1) + "/24"
+                gateway = "10.0." + str(m+2) + ".254"
+                host = self.addHost("h" + str(m*nhost+n+1), cls=RoutedHost, mac=mac, ips=[ip], gateway=gateway)
+                self.addLink("s" + str(LEAF_START_INDEX + m), host)
 
 topos = { 'trellis' : Trellis }
 
@@ -72,7 +85,17 @@ if __name__ == "__main__":
         description='BMv2 mininet demo script (2 by 2 fabric)')
     parser.add_argument('--onos-ip', help='ONOS-BMv2 controller IP address',
                         type=str, action="store", required=True)
+    parser.add_argument('--nleaf', help='Number of leaf switches',
+                        type=int, action="store", default=2, required=False)
+    parser.add_argument('--nspine', help='Number of spine switches',
+                        type=int, action="store", default=2, required=False)
+    parser.add_argument('--nhost', help='Number of host for each leaf switch',
+                    type=int, action="store", default=2, required=False)
     args = parser.parse_args()
     setLogLevel('debug')
+
+    nleaf = args.nleaf
+    nspine = args.nspine
+    nhost = args.nhost
 
     main(args)
